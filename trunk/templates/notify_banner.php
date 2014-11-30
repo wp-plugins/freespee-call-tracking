830253256c5e70feee39cd881a9a712f405b6b7d
effeks <effeks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-30 19:43:38 +0000

    Layout fixes for readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/freespee-call-tracking/tags/1.0@1035591 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/templates/notify_banner.php
@@ -0,0 +1,3 @@
+<div class="update-nag">
+  <p><?php echo sprintf(__('<b>Freespee Call Tracking</b>: Please enter your <a href="%s">customer ID</a> to enable call tracking.', 'freespee_call_tracking'), @admin_url('options-general.php?page=fs_settings_page')); ?></p>
+</div>
